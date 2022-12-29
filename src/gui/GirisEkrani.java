23a2d0379314f2b710e217ebbf0f49f059a5b63e
burhanmutlu <burhackmutlu@gmail.com>
2022-12-30 01:42:31 +0300

    maven projesine çevrildi

--- src/gui/GirisEkrani.java
+++ src/gui/GirisEkrani.java
@@ -5,14 +5,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.Component;
 
 public class GirisEkrani extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -26,16 +36,36 @@ public class GirisEkrani extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public GirisEkrani() {
+		setTitle("Sweet Bank Giriş Ekranı");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 519, 398);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		JPanel GirisEkraniPanel = new JPanel();
+		GirisEkraniPanel.setBackground(new Color(152, 251, 152));
+		contentPane.add(GirisEkraniPanel, "1, 1, 2, 2, fill, fill");
+		
+		textField = new JTextField();
+		GirisEkraniPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("SWEET BANKAYA HOŞ GELDİNİZ.");
+		lblNewLabel.setForeground(new Color(255, 69, 0));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 22));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setSize(new Dimension(375, 375));
+		GirisEkraniPanel.add(lblNewLabel);
+		this.setLocationRelativeTo(null); // pencereyi ortalamak için kullanıyoruz
 	}
 
 }
