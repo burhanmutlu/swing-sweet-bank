db9e88c5796058d007761b6e9b114d8e143ae660
burhanmutlu <burhackmutlu@gmail.com>
2022-12-30 20:25:40 +0300

    layout eklendi

--- src/gui/GirisEkrani.java
+++ src/gui/GirisEkrani.java
@@ -3,32 +3,26 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import java.awt.Color;
 import javax.swing.JLabel;
-import java.awt.Dimension;
-import javax.swing.SwingConstants;
-import java.awt.Font;
-import javax.swing.JTextField;
-import java.awt.Component;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
 
-public class GirisEkrani extends JFrame {
+public class GirisEkrani extends javax.swing.JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField textField;
+	private JFrame frmSweetBankGiri;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GirisEkrani frame = new GirisEkrani();
-					frame.setVisible(true);
+					GirisEkrani window = new GirisEkrani();
+					window.frmSweetBankGiri.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,36 +30,52 @@ public class GirisEkrani extends JFrame {
 		});
 	}
 
+	/**
+	 * Create the application.
+	 */
 	public GirisEkrani() {
-		setTitle("Sweet Bank Giriş Ekranı");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 519, 398);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("default:grow"),}));
-		
-		JPanel GirisEkraniPanel = new JPanel();
-		GirisEkraniPanel.setBackground(new Color(152, 251, 152));
-		contentPane.add(GirisEkraniPanel, "1, 1, 2, 2, fill, fill");
-		
-		textField = new JTextField();
-		GirisEkraniPanel.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("SWEET BANKAYA HOŞ GELDİNİZ.");
-		lblNewLabel.setForeground(new Color(255, 69, 0));
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 22));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setSize(new Dimension(375, 375));
-		GirisEkraniPanel.add(lblNewLabel);
+		initialize();
 		this.setLocationRelativeTo(null); // pencereyi ortalamak için kullanıyoruz
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmSweetBankGiri = new JFrame();
+		frmSweetBankGiri.setTitle("Sweet Bank Giriş Ekranı");
+		frmSweetBankGiri.setBounds(100, 100, 425, 250);
+		frmSweetBankGiri.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel GirisEkraniPanel = new JPanel();
+		GirisEkraniPanel.setBackground(new Color(152, 251, 152));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GroupLayout gl_GirisEkraniPanel = new GroupLayout(GirisEkraniPanel);
+		gl_GirisEkraniPanel.setHorizontalGroup(
+			gl_GirisEkraniPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_GirisEkraniPanel.createSequentialGroup()
+					.addGap(160)
+					.addComponent(lblNewLabel)
+					.addContainerGap(203, Short.MAX_VALUE))
+		);
+		gl_GirisEkraniPanel.setVerticalGroup(
+			gl_GirisEkraniPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_GirisEkraniPanel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(153, Short.MAX_VALUE))
+		);
+		GirisEkraniPanel.setLayout(gl_GirisEkraniPanel);
+		GroupLayout groupLayout = new GroupLayout(frmSweetBankGiri.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(GirisEkraniPanel, GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(GirisEkraniPanel, GroupLayout.DEFAULT_SIZE, 211, Short.MAX_VALUE)
+		);
+		frmSweetBankGiri.getContentPane().setLayout(groupLayout);
+	}
 }
